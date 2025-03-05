3096755cf2cf6b7140d7ef00e107b1d00187851a
Jesse Ezell <jesse.ezell@timu.com>
2025-03-05 10:15:52 -0800

    stripe billing

--- meshagent/api/messaging.py
+++ meshagent/api/messaging.py
@@ -25,8 +25,9 @@ def pack_message(*, header: dict, data:bytes|None = None) -> bytes:
 
 
 class Response(ABC):
-    def __init__(self):
-        pass
+    def __init__(self, *, usage: Optional[dict[str,float]] = None):
+        self.usage = usage
+
     
     @abstractmethod
     def to_json(self):
@@ -40,8 +41,8 @@ class Response(ABC):
 response_types = dict[str, type]()
 
 class LinkResponse(Response):
-    def __init__(self, *, url: str, name: str):
-        super().__init__()
+    def __init__(self, *, url: str, name: str, usage: Optional[dict[str,float]] = None):
+        super().__init__(usage=usage)
         self.name = name
         self.url = url
 
@@ -49,24 +50,25 @@ class LinkResponse(Response):
         return {
             "type" : "link",
             "name" : self.name,
-            "url" : self.url
+            "url" : self.url,
+            "usage" : self.usage
         }
 
     @staticmethod
     def unpack(*, header: dict, payload: bytes):
-        return LinkResponse(name=header["name"], url=header["url"])
+        return LinkResponse(name=header["name"], url=header["url"], usage=header.get("usage", None))
 
     def pack(self):
         return pack_message(header=self.to_json())
     
     def __str__(self):
-        return f"LinkResponse name={self.name}, type={self.url}"
+        return f"LinkResponse name={self.name}, type={self.url} usage={self.usage}"
     
 response_types["link"] = LinkResponse
 
 class FileResponse(Response):
-    def __init__(self, *, data: bytes, name: str, mime_type: str):
-        super().__init__()
+    def __init__(self, *, data: bytes, name: str, mime_type: str, usage: Optional[dict[str,float]] = None):
+        super().__init__(usage=usage)
         self.data = data
         self.name = name
         self.mime_type = mime_type
@@ -75,94 +77,98 @@ class FileResponse(Response):
         return {
             "type" : "file",
             "name" : self.name,
-            "mime_type" : self.mime_type
+            "mime_type" : self.mime_type,
+            "usage" : self.usage
         }
 
     @staticmethod
     def unpack(*, header: dict, payload: bytes):
-        return FileResponse(data=payload, name=header["name"], mime_type=header["mime_type"])   
+        return FileResponse(data=payload, name=header["name"], mime_type=header["mime_type"], usage=header.get("usage", None))   
     
     def pack(self):
         return pack_message(header=self.to_json(), data=self.data)
     
     def __str__(self):
-        return f"FileResponse name={self.name}, type={self.mime_type}, length={len(self.data)}"
+        return f"FileResponse name={self.name}, type={self.mime_type}, length={len(self.data)} usage={self.usage}"
 
 response_types["file"] = FileResponse
 
 class TextResponse(Response):
-    def __init__(self, *, text: str):
-        super().__init__()
+    def __init__(self, *, text: str, usage: Optional[dict[str,float]] = None):
+        super().__init__(usage=usage)
         self.text = text
 
     @staticmethod
     def unpack(*, header: dict, payload: bytes):
-        return TextResponse(text=header["text"])
+        return TextResponse(text=header["text"], usage=header.get("usage", None))
 
     def to_json(self):
         return {
             "type" : "text",
             "text" : self.text,
+            "usage" : self.usage
         }
 
     def pack(self):
         return pack_message(header=self.to_json())
 
     def __str__(self):
-        return f"TextResponse {self.text}"
+        return f"TextResponse text={self.text} usage={self.usage}"
 
 response_types["text"] = TextResponse
 
 class EmptyResponse(Response):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *, usage: Optional[dict[str,float]] = None):
+        super().__init__(usage=usage)
 
     
     def to_json(self):
         return {
             "type" : "empty",
+            "usage" : self.usage
         }
         
 
     @staticmethod
     def unpack(*, header: dict, payload: bytes):
-        return EmptyResponse()
+        return EmptyResponse(usage=header.get("usage", None))
 
     def pack(self):
         return pack_message(header=self.to_json())
     
     def __str__(self):
-        return f"EmptyResponse"
+        return f"EmptyResponse usage={self.usage}"
 
 response_types["empty"] = EmptyResponse
 
 class ErrorResponse(Response):
-    def __init__(self, *, text: str):
-        super().__init__()
+    def __init__(self, *, text: str, usage: Optional[dict[str,float]] = None):
+        super().__init__(usage=usage)
         self.text = text
 
     def to_json(self):
         return {
             "type" : "error",
             "text" : self.text,
+            "usage" : self.usage,
         }
 
     @staticmethod
     def unpack(*, header: dict, payload: bytes):
-        return ErrorResponse(text=header["text"])
+        return ErrorResponse(text=header["text"], usage=header.get("usage", None))
 
     def pack(self):
         return pack_message(header=self.to_json())
     
     def __str__(self):
-        return f"ErrorResponse: {self.text}"
+        return f"ErrorResponse: text={self.text} usage={self.usage}"
     
 
 response_types["error"] = ErrorResponse
 
 class JsonResponse(Response):
-    def __init__(self, json: dict):
-        super().__init__()
+    def __init__(self, *, json: dict, usage: Optional[dict[str,float]] = None):
+        super().__init__(usage=usage)
         self.json = json
 
     def __getitem__(self, name: str):
@@ -172,17 +178,18 @@ class JsonResponse(Response):
         return {
             "type" : "json",
             "json" : self.json,
+            "usage" : self.usage
         }
     
     @staticmethod
     def unpack(*, header: dict, payload: bytes):
-        return JsonResponse(header["json"])
+        return JsonResponse(json=header["json"], usage=header.get("usage", None))
 
     def pack(self):
         return pack_message(header=self.to_json())
     
     def __str__(self):
-        return f"JsonResponse: {json.dumps(self.json)}"
+        return f"JsonResponse: json={json.dumps(self.json)} usage={self.usage}"
 
 
 response_types["json"] = JsonResponse
