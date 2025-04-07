2cb95f0589b4efe9b46052ae6630336c7dd514b3
Jesse Ezell <jesse.ezell@timu.com>
2025-04-07 13:36:58 -0700

    refactor room internals to remove meshagent.tools dependency

--- meshagent/api/__init__.py
+++ meshagent/api/__init__.py
@@ -1,5 +1,5 @@
 from .websocket_protocol import WebSocketClientProtocol
-from .room_server_client import RequiredToolkit, RequiredSchema, Requirement, RoomClient, RoomMessage, RoomException, ToolDescription, ToolkitDescription, RemoteParticipant, LocalParticipant, MeshDocument, FileHandle, MessageStreamReader, MessageStreamWriter, MessageStreamChunk, StorageEntry
+from .room_server_client import RequiredToolkit, RequiredSchema, Requirement, RoomClient, RoomMessage, RoomException, ToolDescription, ToolkitDescription, RemoteParticipant, LocalParticipant, MeshDocument, FileHandle, MessageStreamReader, MessageStreamWriter, MessageStreamChunk, StorageEntry, AgentDescription
 from .participant_token import ParticipantToken, ParticipantGrant
 from .participant import Participant
 from .schema_document import MeshSchema, Element, ElementType, ChildProperty, ValueProperty
