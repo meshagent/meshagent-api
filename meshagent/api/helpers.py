1042fdf9e6286631c7e4d0b49f6d5252791fe483
Jesse Ezell <jesse.ezell@timu.com>
2025-11-12 10:11:38 -0800

    fix websocket room url

--- meshagent/api/helpers.py
+++ meshagent/api/helpers.py
@@ -42,7 +42,7 @@ def websocket_room_url(
                 api_url = "ws:" + api_url.removeprefix("http:")
             base_url = api_url
 
-    return f"{base_url}/rooms/{room_name}?create={'true' if create else 'false'}"
+    return f"{base_url}/rooms/{room_name}"
 
 
 @deprecated("create a ParticipantToken directly instead")
