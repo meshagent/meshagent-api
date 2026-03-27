2a3f867b3ab2ed038cc69ca7bb36be85953d530b
jesse ezell <jezell@gmail.com>
2026-03-27 13:19:54 -0700

    add gke proxy

--- meshagent/api/http.py
+++ meshagent/api/http.py
@@ -8,7 +8,10 @@ import certifi
 
 
 def new_client_session(*args: Any, **kwargs: Any) -> ClientSession:
+    ca_file = kwargs.pop("ca_file", None)
     if "connector" not in kwargs:
         ssl_context = ssl.create_default_context(cafile=certifi.where())
+        if ca_file is not None:
+            ssl_context.load_verify_locations(cafile=ca_file)
         kwargs["connector"] = TCPConnector(ssl=ssl_context)
     return ClientSession(*args, **kwargs)
