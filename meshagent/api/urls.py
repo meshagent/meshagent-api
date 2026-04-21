be9d5b15c2dd6f6678b3a744a4585bae2d869382
jesse ezell <jezell@gmail.com>
2026-04-21 09:55:42 -0700

    add local settings

--- meshagent/api/urls.py
+++ meshagent/api/urls.py
@@ -3,12 +3,27 @@ import os
 
 
 def meshagent_base_url(base_url: Optional[str] = None):
-    return os.getenv("MESHAGENT_API_URL", "https://api.meshagent.com")
+    if base_url is not None:
+        return base_url.rstrip("/")
+
+    profile_api_url = os.getenv("MESHAGENT_PROFILE_API_URL")
+    if profile_api_url:
+        return profile_api_url.rstrip("/")
+
+    env_api_url = os.getenv("MESHAGENT_API_URL")
+    if env_api_url:
+        return env_api_url.rstrip("/")
+
+    return "https://api.meshagent.com"
 
 
 def websocket_room_url(*, room_name: str, base_url: Optional[str] = None) -> str:
     if base_url is None:
-        api_url = os.getenv("MESHAGENT_ROOM_URL", os.getenv("MESHAGENT_API_URL"))
+        api_url = os.getenv("MESHAGENT_ROOM_URL")
+        if api_url is None:
+            api_url = os.getenv("MESHAGENT_PROFILE_API_URL") or os.getenv(
+                "MESHAGENT_API_URL"
+            )
         if api_url is None:
             base_url = "wss://api.meshagent.com"
         else:
